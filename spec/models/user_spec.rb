9dc591b9d1375d7cd4b2f79dae482447b5f9afad
peoplewhite <shonshon7@gmail.com>
2016-11-10 22:01:00 +0800

    [add] add username property to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  role                   :integer
+#  username               :string
 #
 
 require 'rails_helper'
