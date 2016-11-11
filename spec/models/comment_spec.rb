7ab77bca056734ba6a11e6db1e4732a2eef41a96
peoplewhite <shonshon7@gmail.com>
2016-11-11 21:42:46 +0800

    [add] add test for model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,7 +15,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'associations' do
-    it { should belong_to(:problem) }
+    it { should belong_to(:commentable) }
     it { should belong_to(:user) }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,5 +15,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:diary) }
+    it { should belong_to(:project) }
+  end
+
 end
