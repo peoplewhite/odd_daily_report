290daceaea95e5cfa7d9aa1b11a04bc7ac0527a7
peoplewhite <shonshon7@gmail.com>
2016-11-20 15:21:47 +0800

    [add] add new action of diary controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,4 +2,25 @@ class DiariesController < ApplicationController
   def index
     @diaries = Diary.all
   end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
