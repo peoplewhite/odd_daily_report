c2e57199c1150c19fbf1979baef73850ee5283d1
peoplewhite <shonshon7@gmail.com>
2016-11-20 20:29:00 +0800

    [modify] change routes with easier way to handle delete task action

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototype#index'
 
-  resources :diaries do
-    #resources :tasks, only: %i(new edit destroy)
-    resources :tasks
-  end
+  resources :diaries
+  resources :tasks
 
 end
